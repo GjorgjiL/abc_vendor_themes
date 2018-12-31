5c62f6d2110b682ff52e78472efc34ea98c8d9ed
Roger Truttmann <roger.truttmann35@gmail.com>
2018-12-31 19:32:05 +0100

    New ABC bootanimation

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,26 +1,14 @@
 # Boot Animation
 scr_resolution := 1080x1920
-du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
+abc_device := $(patsubst %f,%,$(subst abc_,,$(TARGET_PRODUCT)))
 
-ifneq ($(filter taimen,$(du_device)),)
+ifneq ($(filter taimen,$(abc_device)),)
 scr_resolution := 1440x2880
 endif
 
-ifneq ($(filter crosshatch,$(du_device)),)
-scr_resolution := 1440x2960
-endif
-
-ifneq ($(filter marlin,$(du_device)),)
-scr_resolution := 1440x2560
-endif
-
-ifneq ($(filter bonito sargo,$(du_device)),)
-scr_resolution := 1080x2160
-endif
-
-ifneq ($(filter dragon,$(du_device)),)
-scr_resolution := 1800x2560
-endif
+#ifneq ($(filter crosshatch,$(du_device)),)
+#scr_resolution := 1440x2960
+#endif
 
 ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
 PRODUCT_COPY_FILES += \
